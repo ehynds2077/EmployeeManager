3b5631308a468508e4a80eb0d5c014f196d18a0f
Chris <burd573@gmail.com>
2022-06-07 13:06:19 -0700

    work in progress

---
+++ api/db/knexfile.js
@@ -0,0 +1,18 @@
+import { config } from "../config";
+
+module.exports = {
+  development: {
+    client: "pg",
+    connection: {
+      host: "db",
+      port: 5432,
+      user: config.POSTGRES_USER,
+      password: config.POSTGRES_PASSWORD,
+      database: config.POSTGRES_DB,
+    },
+    acquireConnectionTimeout: 400000,
+    migrations: {
+      directory: "/migrations",
+    },
+  },
+};
